e8a4a5a9fd0255334ff34077732c90eabf447080
nsun94}} <nsun94@charlottesvilleschools.org>
2021-06-08 00:06:36 +0100

    my pics from pi camera

---
+++ pics/piCam.py
@@ -0,0 +1,16 @@
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024, 768)
+    print("Get ready")
+    time.sleep(1)
+    print("3")
+    time.sleep(1)
+    print("2")
+    camera.start_preview()
+    time.sleep(1)
+    print("1")
+    time.sleep(1)
+    print("Taking picture")
+    camera.capture('pic1.jpg')
+    print("Done")
+
+
